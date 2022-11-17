664c2eb5b008aa1b03608cd78ac574ae43800aec
Hiitsu <domkot42@wp.pl>
2022-11-17 23:30:06 +0100

    [ADD] Adding Class to handle connection.

--- src/Client.java
+++ src/Client.java
@@ -1,17 +1,41 @@
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.InetAddress;
-import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
+
+    private static Scanner userInput = new Scanner(System.in);
+    private static String request;
+    private static Connection connection = new Connection();
+
     public static void main(String[] args) throws IOException {
-        connect();
+        connection.connect();
+        System.out.println("What request you want to make? get / set : ");
+        request = userInput.nextLine();
+        key_in(request);
     }
 
-    public static void connect() throws IOException {
-        Socket socket = new Socket(InetAddress.getLocalHost(), 2011);
-        ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+    public static void key_in(String request){
+        int key;
+        switch (request){
+        case "get":
+            System.out.println("Check if following key exists: ");
+            key = userInput.nextInt();
+            try {
+                connection.outputStream.writeObject("key_in-get: " + key);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            break;
+        case "set":
+            System.out.println("Add a key to database: ");
+            key = userInput.nextInt();
+            try {
+                connection.outputStream.writeObject("key_in-set: " + key);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            break;
+        }
+
     }
 }
